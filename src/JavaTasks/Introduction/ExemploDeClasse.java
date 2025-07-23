7112dbea1be1c6d8d7955b9e25754a0a511efb8e
Bruno <brunoisnait@gmail.com>
2025-07-23 20:40:14 -0300

    ExemploDeClasseeComentário

---
+++ src/JavaTasks/Introduction/ExemploDeClasse.java
@@ -0,0 +1,19 @@
+package JavaTasks.Introduction;
+
+
+public class ExemploDeClasse {
+    public static void main(String[] angs){
+        System.out.println("Hello World");
+    }
+}
+
+
+//Exemplo de Comentário
+
+/* Comentário de múltiplas linhas
+
+ * */
+
+/**
+ * Exemplo de comentário <b>javadoc</b>
+ */
