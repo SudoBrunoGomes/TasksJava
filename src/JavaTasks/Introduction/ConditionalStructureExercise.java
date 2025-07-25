736d1c00f5dbba8cfe132f65e486527a6eff0121
Bruno <brunoisnait@gmail.com>
2025-07-25 00:04:18 -0300

    ConditionalStructureEexercise

---
+++ src/JavaTasks/Introduction/ConditionalStructureExercise.java
@@ -0,0 +1,25 @@
+package JavaTasks.Introduction.Image;
+
+//Organização de categorias
+//idade < 15         --> categoria infantil
+//idade > 15 && < 18 --> categoria Juvenil
+//idade >= 18        --> categoria Adulto
+
+
+public class ConditionalStructureExercise {
+    public static void main(String[] args) {
+        int age = 16;
+        String categoria = " ";
+
+        if(age < 15){
+            categoria = "Categoria --> Infantil";
+        } else if (age > 15 && age < 18) {
+            categoria = "Categoria --> Juvenil";
+        }else {
+            categoria = "Categoria --> Adulto";
+        }
+        System.out.println(categoria);
+
+
+    }
+}
